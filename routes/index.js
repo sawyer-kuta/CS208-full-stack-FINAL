ba04c228fd0a347eeee7144abfc024f093c2bc01
sawyer-kuta <sawyerkuta@u.boisestate.edu>
2026-04-29 00:38:28 +0000

    Added all four page routes and basic pug templates

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next){
-  try {
-    req.db.query('SELECT * FROM todos;', (err, results) => {
-      if (err) {
-        console.error('Error fetching todos:', err);
-        return res.status(500).send('Error fetching todos');
-      }
-      res.render('index', { title: 'My Simple TODO', todos: results });
-    });
-  } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
-  }
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Downtown Donuts' });
 });
+
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About Us' });
+});
+
+router.get('/menu', function(req, res) {
+  res.render('menu', { title: 'Menu' });
+});
+
+router.get('/comments', function(req, res) {
+  res.render('comments', { title: 'Comments' });
+});
+
 
 router.post('/create', function (req, res, next) {
     const { task } = req.body;
